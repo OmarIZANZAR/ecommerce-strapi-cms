8ef4acdb0a1fe5865841949a292915fc1d88f6b9
izanzar omar <izanzar.omar@gmail.com>
2022-10-10 23:46:57 +0100

    adding new server

--- config/middleware.js
+++ config/middleware.js
@@ -5,10 +5,12 @@ module.exports = ({ env }) => ({
             origin: [
                 env('HOST'), 
                 env('CLIENT_URL'),
+                env('SERVER_URL'),
                 'http://localhost:3000',
                 'http://localhost:1337',
-                'https://iz-ecommerce-strapi.herokuapp.com',
+                'https://ecommerce-strapi-cms.onrender.com',
                 'https://iz-ecommerce-strapi.netlify.app',
+                '0.0.0.0'
             ],
         },
     },
