a5f9b63db9f9d9c945a8ab59ce21d8d8333991b5
izanzar omar <izanzar.omar@gmail.com>
2022-10-11 12:00:17 +0100

    add client origin to cors

--- config/middleware.js
+++ config/middleware.js
@@ -6,6 +6,8 @@ module.exports = ({ env }) => ({
                 env('HOST'), 
                 env('CLIENT_URL'),
                 env('SERVER_URL'),
+                'https://ecommerce-react-strapi-cms.netlify.app',
+                'http://localhost:3000'
             ],
         },
     },
